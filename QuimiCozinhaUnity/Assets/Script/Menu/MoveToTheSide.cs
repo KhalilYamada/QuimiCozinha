b2fbbc0583e733275ea80f4575b900cf2d9af249
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2020-11-10 23:23:41 -0300

    Aplicando Arte
    Apliquei as primeiras artes de menu e adicionei os botões (ainda não fazem nada)

---
+++ QuimiCozinhaUnity/Assets/Script/Menu/MoveToTheSide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveToTheSide : MonoBehaviour
+{
+
+    private bool wasClicked = false;
+    public Transform lugar;
+
+    float target = 10;
+
+    public void Update()
+    {
+        if (wasClicked)
+        {
+            float xPos = Mathf.Lerp(transform.position.x, target, 0.02f);            
+            transform.position = new Vector3(xPos, 0.0f, 0.0f);
+        }
+    }
+
+
+
+    public void Move()
+    {
+        wasClicked = true;
+    }
+}
